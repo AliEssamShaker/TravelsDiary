93b6c62f97499189ff16dd13b9ab50588b1cf97e
Ali Essam Shaker <59261797+AliEssamShaker@users.noreply.github.com>
2021-03-01 12:24:51 -0700

    Add files via upload

---
+++ models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+
+# class PostImage(models.Model):
+#     post = models.ForeignKey(Post, default=None, on_delete=models.CASCADE)
+#     images = models.FileField(upload_to='images/')
+
+#     def __str__(self):
+#         return self.post.title
